ae9216d43f545c54d7239760a3be73559f0e5399
Jaebum Lee <jaebumlee@Jaebums-MacBook-Pro.local>
2023-02-06 12:37:00 -0500

    dark mode styling

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between sticky top-0">
-            <h1 className="text-xl">developedbybrian</h1>
+            <h1 className="text-xl dark:text-gray-300">developedbybrian</h1>
             <ul className="flex items-center">
               <li>
                 {darkMode ?
@@ -68,28 +68,28 @@ export default function Home() {
             <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
               Brian Lee
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl">
+            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
               Software Developer
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-gray-300">
               Welcome to my portfolio, feel free to connect with me!
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-100">
             <a href="https://www.linkedin.com/in/brianjaebum/" target="_blank" rel="noreferrer">
               <AiFillLinkedin />
             </a>
             <a href="https://github.com/jaebum25" target="_blank" rel="noreferrer">
               <AiFillGithub />
             </a>
-            <AiFillYoutube />
+            {/* <AiFillYoutube /> */}
           </div>
         </section>
 
         <section>
           <div className="lg:flex gap-10">
-            <h3 className="text-3xl py-1">About</h3>
-            <p className="text-md py-2 leading-8 text-gray-800">
+            <h3 className="text-3xl py-1 dark:text-white">About</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-300">
               Welcome to the beginning of my coding journey!
               I started by completing my Software Engineering Immersive Bootcamp from 
               <span className="text-teal-500"> General Assembly </span>
@@ -98,7 +98,7 @@ export default function Home() {
             </p>
           </div>
           <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-600">
               <Image src={Spotter} alt="..."/>
               <a target="_blank" rel="noreferrer" href="https://brians-spotter.herokuapp.com" className="text-lg font-medium py-8 pb-2">Spotter</a>
               <p className="py-2">Search and share your favorite songs!</p>
@@ -107,7 +107,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">HTML, CSS</p>
               <p className="text-gray-800 py-1">Deezer API</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-600">
               <Image src={Restaurant} alt="..."/>
               <h3 className="text-lg font-medium py-8 pb-2">Restaurant Reviewer</h3>
               <p className="py-2">Discover new local restaurants!</p>
@@ -116,7 +116,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">HTML, CSS</p>
               <p className="text-gray-800 py-1">AWS S3</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-600">
             <Image src={Recipe} alt="..."/>
               <h3 className="text-lg font-medium py-8 pb-2">Drink Book</h3>
               <p className="py-2">Create new drink recipes!</p>
@@ -125,7 +125,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">HTML, CSS</p>
               <p className="text-gray-800 py-1">Google OAuth 2.0</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-600">
             <Image src={Roulette} alt="..."/>
               <h3 className="text-lg font-medium py-8 pb-2">Ghoulette</h3>
               <p className="py-2">Test your luck in Spooky Roulette!</p>
@@ -136,8 +136,8 @@ export default function Home() {
         </section>
         <section>
           <div>
-          <h3 className="text-3xl py-1">Thanks!</h3>
-          <p className="text-md py-2 leading-8 text-gray-800">
+          <h3 className="text-3xl py-1 dark:text-white">Thanks!</h3>
+          <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-300">
             More content coming soon..
           </p>
           </div>
